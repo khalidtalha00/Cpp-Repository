336ecec3b6e5c248bcdeadcbff6e328cd1dac526
Talha Khalid <117529116+khalidtalha00@users.noreply.github.com>
2024-12-07 16:31:36 +0530

    Initial Commit

---
+++ FirstProgram.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // header file library for working with input and output objects such as cout
+using namespace std;  // using namespace std meand we can use names for objects and variables.
+int main(){
+    cout << "Hello World";  //cout (pronounced "see-out") is an object used together with the insertion operator (<<) to output/print text.
+    return 0;  // this ends the main function
+}
